73571daa712ce3ccffcb2c610cc8b65e59c05377
Patrick Chiafullo <patrick.chiafullo@gmail.com>
2022-05-20 18:43:18 -0500

    js file add and wip code

---
+++ Downloads/JavaScript-Lab Resources 2/Accordion/js-accordion.js
@@ -0,0 +1,14 @@
+const question = document.querySelectorAll(".question");
+
+question.forEach(question => {
+    question.addEventListener("click", event => {
+        question.classList.toggle("active");
+        const answerHidden = question.querySelectorAll(".answer");
+        if(question.classList.contains("active")) {
+            answerHidden.style.maxHeight = question.scrollHeight + "px";
+        }
+        else {
+            answerHidden.style.maxHeight = 0;
+        }
+    });
+});
